d122ff46eb99d9484e41e086e2a8e498f3fff0d1
iamanshulgit <anshulrng90@gmail.com>
2021-08-19 13:34:19 -0400

    LinkedList element from the last

---
+++ LinkedListExample/KthElementFromBack.java
@@ -0,0 +1,43 @@
+package LinkedListExample;
+
+import java.util.*;
+public class KthElementFromBack {
+	//using extra space
+	private static void solution1(Node a,int k) {
+		if(a == null)
+			return;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		while(a.next != null) {
+			list.add(a.data);
+			a = a.next;
+		}
+		list.add(a.data);
+		System.out.println(list.get(list.size()-k));
+	}
+	//using recursive function
+	private static int solution2(Node a, int k) {
+		if(a==null)
+			return 0;
+		int i = solution2(a.next,k)+1;
+		if(i == k) {
+			System.out.println(a.data);
+		}
+		return i;
+	}
+
+	public static void main(String[] args) {
+		Node a = new Node();
+		a.add(0);
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		a.add(4);
+		a.add(5);
+		a.add(6);
+		a.add(7);
+		solution1(a,1);
+		
+		int s =solution2(a,4);
+	}
+
+}
